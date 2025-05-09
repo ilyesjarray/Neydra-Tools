acfa4244aa409f3fc697dba59e1433a6e7501b8b
ilyesjarray <jarrayilyes18@gmail.com>
2025-05-09 20:12:41 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
       launchScreen.classList.add("hidden");
       linkingScreen.classList.remove("hidden");
-    }, 3000);
+    }, 10000);
   
     // Simulate USB Detection
     let tetheringDetected = false;
